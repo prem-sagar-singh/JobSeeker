66f74ad9ceb13493561d60fbc914e39414d10aac
prem-sagar-singh <tinkues@gmail.com>
2025-11-10 03:26:27 +0530

    header menu and css added

---
+++ job.helper.ui/src/components/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./HomePage.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="home-page">
+      <Header />
+    </div>
+  );
+};
+export default HomePage;
