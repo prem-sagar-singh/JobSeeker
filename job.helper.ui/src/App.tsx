ac110fe14a8f6905223bc4b79116f2d3f78be0eb
prem-sagar-singh <64727378+prem-sagar-singh@users.noreply.github.com>
2025-11-10 03:28:25 +0530

    Revert "Header menu and css"

--- job.helper.ui/src/App.tsx
+++ job.helper.ui/src/App.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
-import HomePage from "./components/Home/HomePage";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
